b6ed69fe5cf3aaf0dc28e717c9ef81f9680c4088
Jheeseong <gmltjd6801@gmail.com>
2022-07-03 13:02:30 +0900

    notice

--- src/main/java/notice/notice/controller/BoardController.java
+++ src/main/java/notice/notice/controller/BoardController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
@@ -21,7 +22,7 @@ public class BoardController {
     private final MemberService memberService;
 
     @GetMapping("/boards/new")
-    public String createBoard(Model model) {
+    public String createBoard(@ModelAttribute("member") Member member, Model model) {
         model.addAttribute("boardForm", new BoardForm());
         return "boards/createBoardForm";
     }
